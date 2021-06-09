9a39073f8a789f0ab5bcb77cdd1a91f3f7a9148f
Arnab Dey <deyarnab13@yahoo.com>
2021-06-09 21:16:09 +0530

    Create revstr.py

---
+++ revstr.py
@@ -0,0 +1,9 @@
+#Implementation of Reverse String using Recursion in Python
+
+def reverse(string):
+    if len(string) == 0:
+        return string
+    else:
+        return reverse(string[1:]) + string[0]
+a = str(input("Enter the string to be reversed: "))
+print(reverse(a))
